ce649f3345efbe65cba0ab9df397bfe8c757f671
Zach Moore <iam.zachjmoore@gmail.com>
2022-06-25 13:01:52 -0600

    Reorganize

---
+++ apps/express_two/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get("/echo", (req, res) => {
+  res.send(req.query);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
