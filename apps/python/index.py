0f4ecbe0e8e9893c5c9bf277f99b9ee88d0af0a2
Zach Moore <z.moore@gosolo.io>
2022-07-05 15:45:18 -0600

    Cleanup

--- apps/python/index.py
+++ apps/python/index.py
@@ -1,7 +1,17 @@
-import time
-
 from utils.index import addNumbers
 
+
+def fibonacci():
+  a, b = 0, 1
+  while True:
+    yield a
+    a, b = b, a + b
+
+
+fibNumbers = fibonacci()
+fibRange = []
+for i in range(10):
+  fibRange.append(next(fibNumbers))
 
 class Pet:
   def __init__(self, name, favoriteNumber):
@@ -15,34 +25,12 @@ class Pet:
     return self._favoriteNumber
 
 
-def fibonacci():
-  a, b = 0, 1
-  while True:
-    yield a
-    a, b = b, a + b
+pets = []
+for index, number in enumerate(fibRange):
+  pets.append(Pet("Pet " + str(index), number))
 
-runCount = 0
-while True:
-
-  print(addNumbers(runCount, "1"))
-  
+for pet in pets:
+  print(pet.name() + " has a favorite number of " + str(pet.favoriteNumber()))
 
 
-  fibNumbers = fibonacci()
-  fibRange = []
-  for i in range(10):
-    fibRange.append(next(fibNumbers))
-
-
-
-  pets = []
-  for index, number in enumerate(fibRange):
-    pets.append(Pet("Pet " + str(index), number))
-
-  for pet in pets:
-    print(pet.name() + " has a favorite number of " + str(pet.favoriteNumber()))
-
-
-
-  runCount += 1
-  time.sleep(10)
+print(addNumbers(390, "610"))
