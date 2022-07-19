7317111bed72fdb498131e27186356b8a8d3bde9
Zach Moore <iam.zachjmoore@gmail.com>
2022-07-19 15:48:55 -0600

    Add working with files

--- apps/python/index.py
+++ apps/python/index.py
@@ -1,6 +1,10 @@
 from utils.index import addNumbers
 
+# using import ---------------------------------------------------------------
+print(addNumbers(390, "610"))
 
+
+# fibonacci ---------------------------------------------------------------
 def fibonacci():
   a, b = 0, 1
   while True:
@@ -13,6 +17,8 @@ fibRange = []
 for i in range(10):
   fibRange.append(next(fibNumbers))
 
+
+#  classes ---------------------------------------------------------------
 class Pet:
   def __init__(self, name, favoriteNumber):
     self._name = name
@@ -32,5 +38,7 @@ for index, number in enumerate(fibRange):
 for pet in pets:
   print(pet.name() + " has a favorite number of " + str(pet.favoriteNumber()))
 
+# reading text file ---------------------------------------------------------------
 
-print(addNumbers(390, "610"))
+file = open("test/file.txt", mode="rt")
+print(file.read())
